570ba52246741ba5e78c1bdcfdbe3fd8e737e7c8
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2023-10-21 19:38:01 +0600

    dynamic area calculation added

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 function calculateTringleArea() {
-    const tringleBase = document.getElementById('triangle-base').value
-    const tringleHight = document.getElementById('triangle-hight').value
+  const tringleBase = document.getElementById("triangle-base").value;
+  const tringleHight = document.getElementById("triangle-hight").value;
 
-    const tringleArea = 0.5 * tringleBase * tringleHight
+  const tringleArea = 0.5 * tringleBase * tringleHight;
 
-    console.log(tringleArea)
+  console.log(tringleArea);
+  addToCalculationEntry("tringle", tringleArea);
 }
 
 function calculateRectangleArea() {
-    const rectangleWidth = document.getElementById('rectangle-width').value
-    const rectangleHeight = document.getElementById('rectangle-height').value
+  const rectangleWidth = document.getElementById("rectangle-width").value;
+  const rectangleHeight = document.getElementById("rectangle-height").value;
 
-    const rectangleArea = rectangleHeight * rectangleWidth
+  const rectangleArea = rectangleHeight * rectangleWidth;
 
-    console.log(rectangleArea)
+  console.log(rectangleArea);
 }
 
 function calculateParallelogramArea() {
-    const ParallelogramWidth = document.getElementById('Parallelogram-base').value
-    const ParallelogramHeight = document.getElementById('Parallelogram-height').value
+  const ParallelogramWidth = document.getElementById("Parallelogram-base")
+    .value;
+  const ParallelogramHeight = document.getElementById("Parallelogram-height")
+    .value;
 
-    const ParallelogramArea = ParallelogramWidth * ParallelogramHeight
+  const ParallelogramArea = ParallelogramWidth * ParallelogramHeight;
 
-    console.log(ParallelogramArea)
+  console.log(ParallelogramArea);
 }
 
-function addToCalculationEntry(areaType, area){
-    console.log(areaType+ ' '  + area)
-    const calculationEntry = document.getElementById('calculation-entry');
+function addToCalculationEntry(areaType, area) {
+  const calculationCount = document.getElementById("calculation-entry");
 
-    const count = calculationEntry.childElementCount;
+  const count = calculationCount.childElementCount;
 
-    const p = document.createElement('p');
-    p.classList.add('my-4');
-    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;
+    const p = document.createElement("p");
+    p.classList.add('flex', 'justify-between')
 
-    calculationEntry.appendChild(p);
+  p.innerHTML =  `<div> ${count +
+    1}. ${areaType} ${area} cm<sup>2</sup></div> <button class="bg-blue-400 py-1 px-2 rounded text-white">
+    Convert to m<sup>2</sup>
+  </button>`;
+
+  calculationCount.appendChild(p);
 }
-    
